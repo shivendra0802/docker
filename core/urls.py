0f9558c402ba9bc2cd94355affc1eec054879a60
shivendra0802 <cheems@kumshiv166@gmail.com>
2021-05-18 18:19:45 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import home
+from app import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+]
